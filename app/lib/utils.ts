f6a1567ee90fabaaf73b0d9e4c72ffb5effd986c
George <71434803+george-swift@users.noreply.github.com>
2025-01-26 19:37:31 +0100

    Centralize client's contact platforms

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,13 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+import {
+  LinkedInIcon,
+  MailIcon,
+  MediumIcon,
+  UpworkIcon
+} from '~/components/icons'
+
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
 
 export const navigation = [
@@ -8,3 +15,22 @@ export const navigation = [
   { name: 'About', href: '/about' },
   { name: 'Contact', href: '/contact' }
 ]
+
+export const socials = {
+  Mail: { name: 'Mail', href: 'mailto:fortuneitok@gmail.com', icon: MailIcon },
+  Linkedin: {
+    name: 'Linkedin',
+    href: 'https://www.linkedin.com/in/fortune-george-itok/',
+    icon: LinkedInIcon
+  },
+  Medium: {
+    name: 'Medium',
+    href: 'https://medium.com/@fortunegeorge',
+    icon: MediumIcon
+  },
+  Upwork: {
+    name: 'Upwork',
+    href: 'https://www.upwork.com/freelancers/~01300baf3645e47f9f?mp_source=share',
+    icon: UpworkIcon
+  }
+}
