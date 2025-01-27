e3fb65bcc977af9c9c5b28c9eeb420f7277a796b
George <71434803+george-swift@users.noreply.github.com>
2025-01-28 00:05:18 +0100

    Update outputs value

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "bucket_website_url" {
-  value = aws_s3_bucket.react_router_site.website_endpoint
+  value = aws_s3_bucket_website_configuration.web-config.website_endpoint
   description = "Static website URL"
 }
